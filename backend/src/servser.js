d194d6b39881ad348b2366d3b11cddca49ac6732
Tarun <tarunpandore8@gmail.com>
2025-07-21 22:23:55 +0530

    Bug fixing

--- backend/src/servser.js
+++ backend/src/servser.js
@@ -5,21 +5,35 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/ratelimiter.js';
 
+import path from "path"
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 // middleware to parse JSON bodies
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
 app.use(rateLimiter);
 
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
